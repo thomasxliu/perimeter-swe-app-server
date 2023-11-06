51f77fcbb1a95dd7648d403e59f98ac9e80e1e54
Thomas Liu <79129497+thomasxliu@users.noreply.github.com>
2023-11-05 19:07:34 -0500

    backend functionality done

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import multer, { StorageEngine } from "multer";
+import multer from "multer";
 import cors from "cors";
-import fs from "fs";
+import sharp from "sharp";
 
 const app = express();
 const PORT = 8080;
@@ -14,27 +14,49 @@ app.use(
   })
 );
 
-const storage: StorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./upload/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
 app.post(
   "/picture",
-  multer({ storage: storage }).single("file"),
-  (req, res) => {
-    // multer will store the file into the local file system
-    console.log(req.file);
-    // TODO: convert image with type not supported by jimp to png
+  multer({ storage: multer.memoryStorage() }).single("file"),
+  async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send("Error. No File Received.");
+    }
 
-    // TODO: add the frame use jimp
+    try {
+      // resize the received image to fit in the frame
+      const metadata = await sharp("./assets/frames/frame1.png").metadata();
+      const resizeWidth: number = metadata.width ? metadata.width * 0.66 : 800;
+      const resizeHeight: number = metadata.height
+        ? metadata.height * 0.66
+        : 1000;
 
-    fs.unlinkSync(`./upload/${req.file?.originalname}`);
-    res.send(req.file);
+      const resizedImage = await sharp(req.file.buffer)
+        .resize({ width: resizeWidth, height: resizeHeight })
+        .png()
+        .toBuffer();
+
+      // we combine image from bottom to top: white background -> received image -> frame
+      const resizedBackgroud = await sharp(
+        "./assets/background/white_background.jpg"
+      )
+        .resize({ width: metadata.width, height: metadata.height })
+        .png()
+        .toBuffer();
+
+      const combinedImage = await sharp(resizedBackgroud)
+        .composite([
+          { input: resizedImage },
+          { input: "./assets/frames/frame1.png" },
+        ])
+        .toBuffer();
+
+      // send the processed image back to frontend
+      res.setHeader("Content-Type", "image/png");
+      res.send(combinedImage);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Error processing the images");
+    }
   }
 );
 
